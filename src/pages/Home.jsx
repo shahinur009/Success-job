6295c66205b31b7541667b612f2744cffe9fa6a0
shahin009 <shaheenmis170879@gmail.com>
2024-05-15 15:29:29 +0600

    Framer Motion use Explore your self section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,20 @@ import FramerMotion from "../components/FramerMotion";
 import JobsCategory from "../components/JobsCategory";
 import OurJobProcess from "../components/OurJobProcess";
 import WeDo from "../components/WeDo";
-import { motion, useScroll } from "framer-motion";
 
 
 
 const Home = () => {
 
     return (
-        
-        <div
-            
 
+        <div
             className="md:container md:px-8 md:py-8 md:mx-auto bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...">
             <Banner />
             <JobsCategory />
             <OurJobProcess />
             <WeDo />
-            <FramerMotion/>
+            <FramerMotion />
 
         </div>
     );
